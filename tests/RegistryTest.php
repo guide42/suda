681cf8f7e975d3ffcd2bee0247913644694eefc1
Juan M <jm@guide42.com>
2014-10-02 01:11:47 -0300

    Unnecessary comment

--- tests/RegistryTest.php
+++ tests/RegistryTest.php
@@ -4,10 +4,6 @@ use Guide42\Suda\Registry;
 
 class RegistryTest extends \PHPUnit_Framework_TestCase
 {
-    // Step 1
-    // Implement a simple array that will contain configuration parameters
-    // that could be whatever.
-
     public function testSettings()
     {
         $registry = new Registry();
