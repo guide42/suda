d08220bfc6be209723de61cee35dbf9f7c56157e
Juan M <jm@guide42.com>
2014-11-22 12:33:58 -0300

    [Loader] Add import with delegate

--- library/Loader/Loader.php
+++ library/Loader/Loader.php
@@ -12,11 +12,36 @@ abstract class Loader implements LoaderInterface
      */
     private $registry;
 
-    public function __construct(RegistryInterface $registry) {
+    /**
+     * @var \Guide42\Suda\Loader\LoaderInterface
+     */
+    private $delegate;
+
+    public function __construct(RegistryInterface $registry,
+                                LoaderInterface $delegate=null) {
+        if ($delegate === null) {
+            $delegate = $this;
+        }
+
         $this->registry = $registry;
+        $this->delegate = $delegate;
     }
 
     public function getRegistry() {
         return $this->registry;
+    }
+
+    public function import($resource) {
+        static $loading = array();
+
+        if (isset($loading[$resource])) {
+            throw new RuntimeException("Cyclic reference for $resource");
+        }
+
+        $loading[$resource] = true;
+
+        $this->delegate->load($resource);
+
+        unset($loading[$resource]);
     }
 }
